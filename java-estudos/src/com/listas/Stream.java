14d523eb1f19a84d9b6639cbe392403f67b810c9
joaopjk <joaopjk48@gmail.com>
2021-06-12 14:39:48 -0300

    Streams

--- java-estudos/src/com/listas/Stream.java
+++ java-estudos/src/com/listas/Stream.java
@@ -1,8 +1,12 @@
+//Documentação: http://docs.oracle.com/javase/8/docs/api/java/util/stream/Stream.html
 package com.listas;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import com.classesAux.Curso;
 
@@ -23,5 +27,21 @@ public class Stream {
 		cursos.stream().map(c -> c.getAlunos()).forEach(System.out::println);
 		int soma = cursos.stream().mapToInt(Curso::getAlunos).sum();
 		System.out.println(soma);
+
+		cursos.stream().filter(c -> c.getAlunos() > 50).forEach(c -> System.out.println(c.getNome()));
+		cursos.stream().map(Curso::getNome).forEach(System.out::println);
+		Optional<Curso> algum = cursos.stream().filter(c -> c.getAlunos() > 50).findAny();
+		Curso umCurso = algum.orElse(null);
+		System.out.println(umCurso);
+		algum.ifPresent(c -> System.out.println(c.getNome()));
+
+		cursos = cursos.stream().filter(c -> c.getAlunos() > 10).collect(Collectors.toList());
+		Map<String, Integer> mapper = cursos.stream().filter(c -> c.getAlunos() > 10)
+				.collect(Collectors.toMap(c -> c.getNome(), d -> d.getAlunos()));
+		System.out.println(mapper);
+
+		cursos.stream().filter(c -> c.getAlunos() > 10).collect(Collectors.toMap(c -> c.getNome(), d -> d.getAlunos()))
+				.forEach((nome, alunos) -> System.out.println("Curso: " + nome + " Alunos: " + alunos));
+		
 	}
 }
