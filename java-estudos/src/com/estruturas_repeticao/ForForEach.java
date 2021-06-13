7211bad09c09bed597d19e961136dbca9f280745
joaopjk <joaopjk48@gmail.com>
2021-06-13 11:47:00 -0300

    ForForEach

--- java-estudos/src/com/estruturas_repeticao/ForForEach.java
+++ java-estudos/src/com/estruturas_repeticao/ForForEach.java
@@ -2,33 +2,22 @@ package com.estruturas_repeticao;
 
 import java.util.Scanner;
 
-public class WhileDoWhile {
+public class ForForEach {
 
 	public static void main(String[] args) {
 		int idade;
 		String nome;
 		Scanner teclado = new Scanner(System.in);
 
-		do {
+		for (int i = 0; i < 5; i++) {
 			System.out.println("Informe seu nome: ");
 			nome = teclado.nextLine();
 			System.out.println("Informe sua idade: ");
 			idade = teclado.nextInt();
 
 			System.out.println("Nome: " + nome + " Idade: " + idade);
-
-		} while (idade > 0);
-
-		while (idade > 0) {
-			System.out.println("Informe seu nome: ");
-			nome = teclado.nextLine();
-			System.out.println("Informe sua idade: ");
-			// idade = teclado.nextInt(); Bug
-			idade = Integer.parseInt(teclado.nextLine());
-
-			System.out.println("Nome: " + nome + " Idade: " + idade);
 		}
-		
+
 		teclado.close();
 	}
 
