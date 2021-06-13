c44f578e5b22fc10e81cbcbb0ee8306bc2981f83
joaopjk <joaopjk48@gmail.com>
2021-06-13 11:24:26 -0300

    Switch

--- java-estudos/src/com/estruturas_decisao/Switch.java
+++ java-estudos/src/com/estruturas_decisao/Switch.java
@@ -5,6 +5,7 @@ public class Switch {
 	public static void main(String[] args) {
 		int numero = 5;
 
+		//Funciona sem o brak, mas vai passar em todos os cases. O default é opcional.
 		switch (numero) {
 		case 0:
 			System.out.println("O número é 0");
