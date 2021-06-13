a49cdf4066a8ee0d1d64df35bffbcd03c7c783d8
joaopjk <joaopjk48@gmail.com>
2021-06-13 13:35:59 -0300

    Tipos alfanuméricos

---
+++ java-estudos/src/com/tipos/TiposAlfanumericos.java
@@ -0,0 +1,14 @@
+package com.tipos;
+
+public class TiposAlfanumericos {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		// Tipos primitivos
+		char letra = 'a';// = 97
+		char letra0 = (char) (letra + 1);
+		// Tipos não primitivos
+		String nome = "João Cícero Vicente Sousa";
+	}
+
+}
