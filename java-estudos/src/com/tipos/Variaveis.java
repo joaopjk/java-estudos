020fa16b0f6fdad17e09de8e06ab7508d706ffc1
joaopjk <joaopjk48@gmail.com>
2021-06-13 10:45:35 -0300

    Variaveis

---
+++ java-estudos/src/com/tipos/Variaveis.java
@@ -0,0 +1,20 @@
+package com.tipos;
+
+public class Variaveis {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		// Declaração de variável;
+		int valor;
+		int num1, num2, num3;
+		int num4, num5 = 4, num6 = 8;
+		char caractere;
+		float preço;
+		int num9;
+		int num10 = 10;
+		int valorTotal;
+		float _valor = 34.6f;//Float precisando de declar o f no final do valor;
+		double valor2 = 44.2;
+	}
+
+}
