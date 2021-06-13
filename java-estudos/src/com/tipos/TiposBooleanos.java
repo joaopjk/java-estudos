54a3847d1007779b75250ea3b3bfb8858d0525f4
joaopjk <joaopjk48@gmail.com>
2021-06-13 13:42:42 -0300

    Tipos booleanos

---
+++ java-estudos/src/com/tipos/TiposBooleanos.java
@@ -0,0 +1,14 @@
+package com.tipos;
+
+public class TiposBooleanos {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		boolean verdadeiro = true;
+		boolean falso = false;
+		
+		Boolean v = true;
+		Boolean f = false;
+	}
+
+}
