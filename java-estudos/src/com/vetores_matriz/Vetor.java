acbdcd150d1ce16199c9db396a615a725148ab32
joaopjk <joaopjk48@gmail.com>
2021-06-13 13:58:06 -0300

    Vetor 1

---
+++ java-estudos/src/com/vetores_matriz/Vetor.java
@@ -0,0 +1,20 @@
+package com.vetores_matriz;
+
+import java.util.Iterator;
+
+//São conhecidos como Array(vetores uni-dimensionais)
+public class Vetor {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		int numeros[] = new int[5];
+		for (int i = 0; i < numeros.length; i++) {
+			numeros[0] = i + 1;
+		}
+
+		int numero[] = { 1, 2, 3, 4, 5 };
+		float floats[] = new float[5];
+		String nome[] = new String[4];
+	}
+
+}
