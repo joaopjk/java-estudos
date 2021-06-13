d53a3e9b3a9c2dbe8a17f7a6b9e0fc331fdb84f5
joaopjk <joaopjk48@gmail.com>
2021-06-13 13:48:25 -0300

    Operações matemáticas

---
+++ java-estudos/src/com/tipos/OperacoesMatematicas.java
@@ -0,0 +1,17 @@
+package com.tipos;
+
+public class OperacoesMatematicas {
+
+	public static void main(String[] args) {
+		int numeroa = 12;
+		int numerob = 24;
+
+		System.out.println(numeroa + numerob);
+		System.out.println(numeroa / numerob);
+		System.out.println(numeroa - numerob);
+		System.out.println(numeroa * numerob);
+		System.out.println(numeroa % numerob);
+
+	}
+
+}
