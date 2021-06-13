705f2d9ccba7f42ae32407adb25b005a7a9d13b9
joaopjk <joaopjk48@gmail.com>
2021-06-13 11:22:14 -0300

    Switch

---
+++ java-estudos/src/com/estruturas_decisao/Switch.java
@@ -0,0 +1,25 @@
+package com.estruturas_decisao;
+
+public class Switch {
+
+	public static void main(String[] args) {
+		int numero = 5;
+
+		switch (numero) {
+		case 0:
+			System.out.println("O número é 0");
+			break;
+		case 3:
+			System.out.println("O número é 3");
+			break;
+		case 5:
+			System.out.println("O número é 5");
+			break;
+		default:
+			System.out.println("O número é :" + numero);
+			break;
+		}
+
+	}
+
+}
