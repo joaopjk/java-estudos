334c4baf490d21db43dda2da128519a557bef5d7
joaopjk <joaopjk48@gmail.com>
2021-06-19 14:05:25 -0300

    Construtores

--- java-estudos/src/com/oo/Produto.java
+++ java-estudos/src/com/oo/Produto.java
@@ -16,11 +16,27 @@ package com.oo;
   - Podemos enteder os métodos como a ação que realizada por um objeto da classe
   - Os métodos são comportamentos dos objetos da classe
   - Tipo de retorno | Nome que corresponde a ação do método | Parametros (opcionais) | Retorno
+  
+   *Construtor
+  - O construtor de uma classe SEMPRE tem o mesmo nome da classe;
+  - Por padrão, a JVM, cria em tempo de execução um construtor padrão (vazio)
+  - Podemos ter mais de um construtor na classe
  */
 public class Produto {
 	String nome;
 	float preco;
 	float desconto;
+	
+	public Produto() {
+		super();
+	}
+
+	public Produto(String nome, float preco, float desconto) {
+		super();
+		this.nome = nome;
+		this.preco = preco;
+		this.desconto = desconto;
+	}
 
 	void aumentarPreco(float valor) {
 		this.preco += valor;
