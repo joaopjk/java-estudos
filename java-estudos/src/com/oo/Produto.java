d1f3d26936e4af8ef24233ba3c7242f711b8b0d1
joaopjk <joaopjk48@gmail.com>
2021-06-19 13:32:27 -0300

    Classes

---
+++ java-estudos/src/com/oo/Produto.java
@@ -0,0 +1,12 @@
+package com.oo;
+
+/*Classe
+ - O nome das classes inicia com letra maiúscula:
+ - O nome não deve conter: Acentuação, caracteres especiais,espaço
+ - Toda classe Java, não existe a Main()
+ - O nome da classe Java DEVE ser o mesmo do arquivo java.
+ - Tudo que estiver dentro das "chaves {}" faz parte da chave
+ */
+public class Produto {
+	
+}
