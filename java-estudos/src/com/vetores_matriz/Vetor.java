d9e56b037d57d64200dd13a1113607e95abd36d8
joaopjk <joaopjk48@gmail.com>
2021-06-19 07:30:27 -0300

    Funções

--- java-estudos/src/com/vetores_matriz/Vetor.java
+++ java-estudos/src/com/vetores_matriz/Vetor.java
@@ -9,29 +9,32 @@ public class Vetor {
 
 	public static void main(String[] args) {
 		int qtd;
-		
+
 		System.out.println("Informe a quantidade de frutas para cadastrar: ");
 		qtd = Integer.parseInt(teclado.nextLine());
-		
+
 		cadastrarDado(qtd);
 		mostrarDados();
-		
+
 		teclado.close();
 	}
-	
+
 	static void cadastrarDado(int qtd) {
 		frutas = new String[qtd];
-		
-		for(int i = 0; i < frutas.length;i++) {
-			System.out.println("Informe a " + (1+i)+" fruta :");
+
+		for (int i = 0; i < frutas.length; i++) {
+			System.out.println("Informe a " + (1 + i) + " fruta :");
 			frutas[i] = teclado.nextLine();
 		}
 	}
-	
+
 	static void mostrarDados() {
-		for(int i = frutas.length -1 ; i >= 0;i--) {
+		for (int i = frutas.length - 1; i >= 0; i--) {
 			System.out.println(frutas[i]);
 		}
 	}
 
+	static int soma(int val1, int val2) {
+		return val1 + val2;
+	}
 }
