28b16678038faf1ea382754d3c9c14ecbf51e749
joaopjk <joaopjk48@gmail.com>
2021-06-19 13:37:57 -0300

    Atributos

--- java-estudos/src/com/oo/Produto.java
+++ java-estudos/src/com/oo/Produto.java
@@ -6,7 +6,15 @@ package com.oo;
  - Toda classe Java, não existe a Main()
  - O nome da classe Java DEVE ser o mesmo do arquivo java.
  - Tudo que estiver dentro das "chaves {}" faz parte da chave
+ 
+ *Atributos
+ - São as características da class/molde/modelo de dados:
+ - Atributos podem ser chamados de estados.
+ - Atributos são nomeados em letras minúsculas, sem espaços, sem caracteres especiais, sem acentuação
  */
 public class Produto {
+	String nome;
+	float preco;
+	float desconto;
 	
 }
