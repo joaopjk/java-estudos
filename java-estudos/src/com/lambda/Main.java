ea2a7b55d9ea70ca95d233ba7bad71b3fc2fc2b6
joaopjk <joaopjk48@gmail.com>
2021-06-20 11:13:06 -0300

    Lambdas

--- java-estudos/src/com/lambda/Main.java
+++ java-estudos/src/com/lambda/Main.java
@@ -7,64 +7,73 @@ import java.util.function.Consumer;
 
 public class Main {
 
-    public static void main(String[] args) {
-        List<String> palavras = new ArrayList<>();
-        palavras.add("aluna");
-        palavras.add("editora");
-        palavras.add("hoje");
-        palavras.add("caelum");
+	public static void main(String[] args) {
+		List<String> palavras = new ArrayList<>();
+		palavras.add("aluna");
+		palavras.add("editora");
+		palavras.add("hoje");
+		palavras.add("caelum");
 
-        Consumer<String> consumidor = new Consumer<String>() {
-            @Override
-            public void accept(String s) {
-                System.out.println(s);
-            }
+		Consumer<String> consumidor = new Consumer<String>() {
+			@Override
+			public void accept(String s) {
+				System.out.println(s);
+			}
 
-        };
-        palavras.forEach(consumidor);
+		};
+		palavras.forEach(consumidor);
 
-        palavras.forEach(new Consumer<String>() {
-            @Override
-            public void accept(String s) {
-                System.out.println(s);
-            }
-        });
+		palavras.forEach(new Consumer<String>() {
+			@Override
+			public void accept(String s) {
+				System.out.println(s);
+			}
+		});
 
-        palavras.forEach((String s) -> System.out.println(s));
+		palavras.forEach((String s) -> System.out.println(s));
 
-        palavras.forEach(s -> {
-            System.out.println(s);
-        });
+		palavras.forEach(s -> {
+			System.out.println(s);
+		});
 
-        palavras.forEach(s -> System.out.println(s));
+		palavras.forEach(s -> System.out.println(s));
 
-        palavras.sort(new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                if (s1.length() < s2.length())
-                    return -1;
-                if (s1.length() > s2.length())
-                    return 1;
-                return 0;
-            }
-        });
+		palavras.sort(new Comparator<String>() {
+			@Override
+			public int compare(String s1, String s2) {
+				if (s1.length() < s2.length())
+					return -1;
+				if (s1.length() > s2.length())
+					return 1;
+				return 0;
+			}
+		});
 
-        palavras.sort((s1, s2) -> {
-            if (s1.length() < s2.length())
-                return -1;
-            if (s1.length() > s2.length())
-                return 1;
-            return 0;
-        });
+		palavras.sort((s1, s2) -> {
+			if (s1.length() < s2.length())
+				return -1;
+			if (s1.length() > s2.length())
+				return 1;
+			return 0;
+		});
 
-        palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
 
-        Consumer<String> impressor = s -> System.out.println(s);
-        palavras.forEach(impressor);
+		Consumer<String> impressor = s -> System.out.println(s);
+		palavras.forEach(impressor);
 
-        new Thread(() -> System.out.println("Executando uma ação")).start();
+		new Thread(() -> System.out.println("Executando uma ação")).start();
 
+		palavras.forEach((String s) -> System.out.println(s));
+		palavras.forEach(System.out::println);
+		palavras.forEach(s -> System.out.println(s));
 
-    }
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+
+		Consumer<String> consumidor1 = s -> System.out.println(s);
+		Comparator<String> comparator = (x, y) -> Integer.compare(x.length(), y.length());
+		palavras.forEach(consumidor1);
+		palavras.sort(comparator);
+	}
 
 }
