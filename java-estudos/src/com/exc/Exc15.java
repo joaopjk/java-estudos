589427109e05afe8c5da252b8474d3a78078ba05
joaopjk <joaopjk48@gmail.com>
2021-07-03 16:09:50 -0300

    Switch

---
+++ java-estudos/src/com/exc/Exc15.java
@@ -0,0 +1,44 @@
+package com.exc;
+
+import java.util.Scanner;
+
+public class Exc15 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		String dia;
+
+		System.out.println("Digite o número do dia da semana [1 a 8]: ");
+		int numeroDia = Integer.parseInt(teclado.nextLine());
+
+		switch (numeroDia) {
+		case 1:
+			dia = "domingo";
+			break;
+		case 2:
+			dia = "segunda";
+			break;
+		case 3:
+			dia = "terça";
+			break;
+		case 4:
+			dia = "quarta";
+			break;
+		case 5:
+			dia = "quinta";
+			break;
+		case 6:
+			dia = "sexta";
+			break;
+		case 7:
+			dia = "sabado";
+			break;
+		default:
+			dia = "inválido";
+		}
+
+		System.out.println("O dia da semana é: " + dia);
+		teclado.close();
+	}
+
+}
