3bc84f1a7f7d1f08da76c28d2746020f8d9b5d91
joaopjk <joaopjk48@gmail.com>
2021-07-19 21:44:38 -0300

    LinkedList

--- java-estudos/src/com/listas/LinkedLists.java
+++ java-estudos/src/com/listas/LinkedLists.java
@@ -1,9 +1,17 @@
 package com.listas;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class LinkedLists {
-
+	/*
+	 * Tem indice Garante indice de inserção Lista duplamente encadeada
+	 */
 	public static void main(String[] args) {
-		
+		LinkedList<String> lista = new LinkedList<String>();
+		lista.add("Primeira String");
+		lista.addAll(Arrays.asList("Segunda String", "Terceira String", "Quarta String"));
+		lista.remove(1);
+		lista.size();
 	}
-
 }
