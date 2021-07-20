49adbb10dc444fc9707015bacd350a3e17c90c70
joaopjk <joaopjk48@gmail.com>
2021-07-19 21:52:38 -0300

    Vector

---
+++ java-estudos/src/com/listas/Vectors.java
@@ -0,0 +1,19 @@
+package com.listas;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Vector;
+
+public class Vectors {
+/*
+ * Threadsafe
+ */
+	public static void main(String[] args) {
+		List<String> lista = new Vector<String>();
+		lista.add("Primeira String");
+		lista.addAll(Arrays.asList("Segunda String", "Terceira String", "Quarta String"));
+		lista.remove(1);
+		lista.size();
+	}
+
+}
